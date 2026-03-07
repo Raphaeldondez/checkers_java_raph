f3999e1821b562ef0368fcd6b85f7c76811f8ead
Marc Robison <marc@marcrobison.com>
2026-03-07 12:00:26 +0100

    feat: add cursor click event and coordinate reading
    includes bounds checking for clicks outside the board.
    Does not cover the grid by drawing squares of size
    delta-1

--- src/client/client.java
+++ src/client/client.java
@@ -4,12 +4,17 @@ import shared.board;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.*;
 
 class client extends JFrame {
 	private int port = 5000;
 	private String server_host = "localhost";
+	private int square_clicked = -1;
+	private final int delta = 60;
+	int origin_x;
+	int origin_y;
 
 	public client() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,13 +24,14 @@ class client extends JFrame {
 		setVisible(true);
 	}
 
-	class board_pannel extends JPanel {
+	class board_pannel extends JPanel implements MouseListener {
 		private final int board_pannel_size_pixels = 600;
 		private board b;
 
 		public board_pannel() {
+			addMouseListener(this);
 			b = new board();
-			b.print_board();
+			// b.print_board();
 		}
 
 		public Dimension getPreferredSize() {
@@ -35,41 +41,84 @@ class client extends JFrame {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			g.setColor(Color.gray);
-			final int delta = 60;
 			// center board
 			Dimension d = this.getSize();
-			int x = (d.width - (b.side_size * delta)) / 2;
-			int y = (d.height - (b.side_size * delta)) / 2;
+			origin_x = (d.width - (b.side_size * delta)) / 2;
+			origin_y = (d.height - (b.side_size * delta)) / 2;
 
+			System.out.printf("origin x: %d\norigin y: %d\n", origin_x, origin_y);
+
+			// draw grid
 			for (int line = 0; line <= b.side_size; line++) {
-				g.drawLine(x, y + line * delta, x + b.side_size * delta, y + line * delta);
-				g.drawLine(x + line * delta, y, x + line * delta, y + b.side_size * delta);
+				g.drawLine(origin_x, origin_y + line * delta, origin_x + b.side_size * delta,
+						origin_y + line * delta);
+				g.drawLine(origin_x + line * delta, origin_y, origin_x + line * delta,
+						origin_y + b.side_size * delta);
 			}
 			final int circle_diameter = 2 * delta / 3;
 
+			// draw pieces
 			for (int y_index = 0; y_index < b.side_size; y_index++) {
 				for (int x_index = 0; x_index < b.side_size; x_index++) {
-					if (b.board[x_index + y_index * b.side_size] == b.BLACK_PAWN
+					int board_array_idx = x_index + y_index * b.side_size;
+					if (board_array_idx == square_clicked) {
+						g.setColor(Color.green);
+						g.fillRect(origin_x + 1 + delta * x_index,
+								origin_y + 1 + delta * y_index,
+								delta - 1, delta - 1);
+					}
+					if (b.board[board_array_idx] == b.BLACK_PAWN
 							|| b.board[x_index] == b.BLACK_KING)
 						g.setColor(Color.BLACK);
-					else if (b.board[x_index + y_index * b.side_size] == b.RED_PAWN
+					else if (b.board[board_array_idx] == b.RED_PAWN
 							|| b.board[x_index] == b.RED_KING)
 						g.setColor(Color.RED);
 					else
 						continue;
-					g.fillOval((x - circle_diameter / 2 + delta / 2) + delta * x_index,
-							(y - circle_diameter / 2 + delta / 2) + delta * y_index,
+					g.fillOval((origin_x - circle_diameter / 2 + delta / 2) + delta * x_index,
+							(origin_y - circle_diameter / 2 + delta / 2) + delta * y_index,
 							circle_diameter,
 							circle_diameter);
 				}
 			}
 		}
 
-		// g.setColor(Color.RED);
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
 
-		// Draw Text
-		// g.drawString("This is my custom Panel!", 10, 20);
+		}
 
+		@Override
+		public void mousePressed(MouseEvent e) {
+			Point p = e.getPoint();
+			System.out.printf("x: %d\ny: %d\n", p.x, p.y);
+			if (p.y < b.side_size * delta + origin_y && p.y > origin_y
+					&& p.x < b.side_size * delta + origin_x
+					&& p.x > origin_x) { // make sure click is within bounds
+				square_clicked = ((p.y - origin_y) / delta) * b.side_size + (p.x - origin_x) / delta;
+				System.out.printf("clicked on %d\n", square_clicked);
+				repaint();
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
 	}
 
 	public static void main(String[] args) {
