0a5282104958ec6cc9ffe3b1d936484823a679f0
raphael dondez <raphael59219@hotmail.com>
2026-03-16 10:49:02 +0100

    ajout des classes clientNetwork et server Network pour la communication client serveur et ( connexions, envoi, écoute)

--- src/client/clientNetwork.java
+++ src/client/clientNetwork.java
@@ -2,13 +2,10 @@ package client;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.*;
-import server.server;
 
 public class clientNetwork{
 	private int port = 5000;
--- src/server/serverNetwork.java
+++ src/server/serverNetwork.java
@@ -3,8 +3,6 @@ package server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.*;
